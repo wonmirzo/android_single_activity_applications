d1eca02914c62138c289e7e27ba77ce05f943d31
Mirzohid Dilshodov <md20021203@gmail.com>
2022-07-15 08:39:27 +0500

    ff

---
+++ app/src/main/java/com/wonmirzo/android_saas/activity/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.wonmirzo.android_saas.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.wonmirzo.android_saas.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
