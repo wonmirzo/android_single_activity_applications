4e3dba9eb38c77a9217d8286719e88ab35657df4
Mirzohid Dilshodov <md20021203@gmail.com>
2022-07-15 10:24:07 +0500

    fragment

--- app/src/main/java/com/wonmirzo/android_saas/fragment/main/home/HomeFragment.kt
+++ app/src/main/java/com/wonmirzo/android_saas/fragment/main/home/HomeFragment.kt
@@ -1,7 +1,8 @@
 package com.wonmirzo.android_saas.fragment.main.home
 
 import androidx.fragment.app.Fragment
+import com.wonmirzo.android_saas.R
 
-class HomeFragment : Fragment() {
+class HomeFragment : Fragment(R.layout.fragment_home) {
 
 }
